1c7678ed9122cbcc4144f0662508ad204b4c324f
3ai4ik <stepanishen.petya@gmail.com>
2024-03-29 17:58:02 +0200

    small changes in websockets

--- src/main/java/ua/zai4ik/restFirst/websocket/WebSocketConfig.java
+++ src/main/java/ua/zai4ik/restFirst/websocket/WebSocketConfig.java
@@ -20,9 +20,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOrigins(
-                        "http://fast-pupils.gl.at.ply.gg:12088/",
-                        "http://localhost:3000/",
-                        "http://localhost:25565/");
+                        "http://fast-pupils.gl.at.ply.gg:12088",
+                        "http://localhost:3000",
+                        "http://localhost:25565");
     }
 
 }
