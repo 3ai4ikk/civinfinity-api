b802b93025404cd4ee4ce28a35b1f02f47a74fe4
3ai4ik <stepanishen.petya@gmail.com>
2024-03-29 18:01:54 +0200

    added corsconfig

---
+++ src/main/java/ua/zai4ik/restFirst/CorsConfig.java
@@ -0,0 +1,22 @@
+package ua.zai4ik.restFirst;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*"); // Разрешить запросы от всех источников, лучше использовать конкретные источники
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
