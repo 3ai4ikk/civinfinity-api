9720ac5557775e59fc38d06b42afb0e264aac8d5
3ai4ik <stepanishen.petya@gmail.com>
2024-03-29 18:06:04 +0200

    changes corsconfig

--- src/main/java/ua/zai4ik/restFirst/CorsConfig.java
+++ src/main/java/ua/zai4ik/restFirst/CorsConfig.java
@@ -1,22 +1,17 @@
 package ua.zai4ik.restFirst;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*"); // Разрешить запросы от всех источников, лучше использовать конкретные источники
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/ws/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST")
+                .allowCredentials(false); // Важно установить true, если запросы отправляются с учетом учетных данных
     }
 }
